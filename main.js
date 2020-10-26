80aaba59ebbf2835f339f6101650fb87261ca8af
Maichol <msibiriu@gmail.com>
2020-10-26 17:33:35 +0100

    non torna niente

---
+++ main.js
@@ -0,0 +1,14 @@
+// come aprire menu con un click
+var hamburger = $(".hamburger-menu");
+var list = $(".hamburger-menu ul li a")
+hamburger.click( function() {
+   list.show();
+});
+
+
+
+
+// come chiudere menu con un click
+// hamburger.click( function() {
+//
+// });
