331baf925b2040ffc4405f8d433e23e4aec34cee
Maichol <msibiriu@gmail.com>
2020-10-27 10:03:12 +0100

    modificata apertura e chiusura hamburger con addclass e remove

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 var hamburger = $(".fa-bars");
 var list = $(".hamburger-menu");
 hamburger.click( function() {
-   list.show();
+   list.addClass("active");
 });
 
 
 // come chiudere menu con un click
 var close = $(".close")
 close.click( function() {
-  list.hide();
+  list.removeClass("active");
 });
